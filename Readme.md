aa86b2115a8a1431afd7a18506eaa7b34313cf28
Diana <diana.gerasimchuk1@gmail.com>
2022-06-30 09:56:23 +0200

    Initial comment

---
+++ Readme.md
@@ -0,0 +1,13 @@
+Training project to practice Kubernetes.
+
+
+Used technology:
+1. Node.js
+2. React
+3. Docker, Kubernetes
+
+
+# Installation
+
+Required to install Docker minikube, kubectl.
+
---
+++ client/src/CommentList.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
+    const { status, content: commentContent } = comment;
+
+    if (status === "approved") {
+      content = commentContent;
+    }
+
+    if (status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (status === "rejected") {
+      content = "This comment as been granted rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
