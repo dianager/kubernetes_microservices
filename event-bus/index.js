b474b4178d6fe9ebce05908dfd126b7b5cfeb295
Dziyana Vinakur <dziyanavinakur@MacBook-Pro-Dziyana.local>
2022-06-30 16:01:15 +0200

    Prepared deployments for query,comments, moderation, ingress-service

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,18 +16,18 @@ app.post("/events", (req, res) => {
     // posts
     console.log(err.message);
   });
-  // axios.post("http://localhost:4001/events", event).catch((err) => {
-  //   // comments
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4002/events", event).catch((err) => {
-  //   // query
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4003/events", event).catch((err) => {
-  //   // moderation
-  //   console.log(err.message);
-  // });
+  axios.post("http://comments-cluster-ip-service:4001/events", event).catch((err) => {
+    // comments
+    console.log(err.message);
+  });
+  axios.post("http://query-cluster-ip-service:4002/events", event).catch((err) => {
+    // query
+    console.log(err.message);
+  });
+  axios.post("http://moderation-cluster-ip-service:4003/events", event).catch((err) => {
+    // moderation
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
 });
 
